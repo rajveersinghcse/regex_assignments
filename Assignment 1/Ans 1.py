affdb9a1cddf74693674be77381abfe0a61865f0
Rajveer Singh <37257664+rajveercse@users.noreply.github.com>
2021-12-20 19:14:16 +0530

    Add files via upload

---
+++ Assignment 1/Ans 1.py
@@ -0,0 +1,19 @@
+#Variable Declaration
+a = 45
+b = 35
+c = 50
+d = 90
+
+#Arithmetic formulas
+add = a + b + c + d
+sub = a - b - c - d
+mult = a * b * c * d
+div = a / b / c / d
+
+
+#Printing the Values
+print("This Addition of the Value is : ", add)
+print("This Subtraction of the Value is : ", sub)
+print("This Multiplication of the Value is : ", mult)
+print("This Division of the Value is : ", div)
+
